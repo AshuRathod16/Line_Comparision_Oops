1a818581e31e4b622ad6ca04f1fd85475a77f735
Ashwini Rathod <rashwini510@gmail.com>
2022-06-03 08:48:31 -0700

    Added:Check the two lines are equal or not

--- src/LineComparision.java
+++ src/LineComparision.java
@@ -1,26 +1,39 @@
 import java.util.Scanner;
 public class LineComparision {
-        public static double lineValues() {
-            try (Scanner scanner = new Scanner(System.in)) {
-                double x1,x2,y1,y2;
+    public static double lineValues() {
+        Scanner sc = new Scanner(System.in);
+        double x1, x2, y1, y2;
 
-                System.out.println("Enter Values For First Point");
-                x1 = scanner.nextDouble();
-                y1 = scanner.nextDouble();
+        System.out.println("Enter Values For First Point");
+        x1 = sc.nextDouble();
+        y1 = sc.nextDouble();
 
-                System.out.println("Enter Values For Second Point");
-                x2 = scanner.nextDouble();
-                y2 = scanner.nextDouble();
+        System.out.println("Enter Values For Second Point");
+        x2 = sc.nextDouble();
+        y2 = sc.nextDouble();
 
-                double lineLength = (double) Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-                return lineLength;
-            }
+        double lineLength = (double) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        return lineLength;
+    }
+
+    public static void checkEquality(double lineOneLength, double lineTwoLength) {
+        if (lineOneLength == lineTwoLength) {
+            System.out.println("Lines are Equal");
+        } else {
+            System.out.println("Lines are not Equal");
         }
+    }
 
-        public static void main(String[] args) {
-            System.out.println("Welcome to Line Comparison Computation ");
-            double lineLength = lineValues();
-            System.out.println("Length Of Line is: " + lineLength);
 
-        }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation ");
+
+        double lineOneLength = lineValues();
+        System.out.println("Length Of Line1 is: " + lineOneLength);
+
+        double lineTwoLength = lineValues();
+        System.out.println("Length Of Line2 is: " + lineTwoLength);
+
+        checkEquality(lineOneLength, lineTwoLength);
+    }
 }
